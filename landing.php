4030525b409a00509b168bedb5b2dcddfd08395d
Noah M <nm6ss@virginia.edu>
2019-04-12 17:16:27 -0400

    Landing links

--- landing.php
+++ landing.php
@@ -17,6 +17,12 @@
 	</br>
 	<a href="rangers.php">Rangers</a>
 	</br>
+	<a href="scientist.php">Scientists</a>
+	</br>
+	<a href="location.php">Locations</a>
+	</br>
+	<a href="visitor.php">Visitors</a>
+	</br>
 	<a href="export.php">JSON Export</a>
 	</br>
 	<a href="logout.php">Logout</a>
